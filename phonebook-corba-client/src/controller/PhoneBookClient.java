0d9f0c3134a9f9ee61810169b46514d410279b66
Lucas Diego Reboucas Rocha <lucas.diegorr@gmail.com>
2014-10-06 01:46:26 -0300

    Modificação da estrutura de dados para TreeMap; Implementação dos métodos de atualização e remoção; Ajuste do layout;

--- phonebook-corba-client/src/controller/PhoneBookClient.java
+++ phonebook-corba-client/src/controller/PhoneBookClient.java
@@ -6,9 +6,13 @@ package controller;
 import interfaces.PhoneBookServerInterface;
 import interfaces.PhoneBookServerInterfaceHelper;
 
-import java.util.HashMap;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Properties;
+import java.util.TreeMap;
 
+import org.apache.commons.codec.binary.Base64;
 import org.omg.CORBA.ORB;
 import org.omg.CORBA.Object;
 import org.omg.CORBA.ORBPackage.InvalidName;
@@ -23,54 +27,79 @@ import org.omg.CosNaming.NamingContextPackage.NotFound;
  * @email lucas.diegorr@gmail.com
  * @year 2014
  */
-public class PhoneBookClient {
-	
-	private HashMap<String, Integer> listContacts;
+public class PhoneBookClient implements Runnable{
+
+	private TreeMap<String, Integer> listContacts;
 	private PhoneBookServerInterface server;
 	private String ipNameService;
 	private String portNameService;
 	private ORB orb;
-	
+	private boolean isChanged;
 	/**
 	 * @param ip 
 	 * @param port 
 	 * 
 	 */
-	public PhoneBookClient(String ip, String port) {
+	public PhoneBookClient(String ip) {
 		this.ipNameService = ip;
-		this.portNameService = port;
-		this.setListContacts(new HashMap<String, Integer>());
+		this.portNameService = "900";
+		this.setChanged(false);
+		this.setListContacts(new TreeMap<String, Integer>());
 	}
-	
-	public void init() {
-		
+
+	public boolean init() {
+
 		Properties properties = new Properties();
 		properties.put("org.omg.CORBA.ORBInitialHost", ipNameService);
 		properties.put("org.omg.CORBA.ORBInitialPort", portNameService);
-		
+
 		this.orb = ORB.init((String[])null, properties);
-		System.out.println("Iniciado o ORB");
-		try {
-			Object objectNameService = orb.resolve_initial_references("NameService");
-			
-			NamingContext naming = NamingContextHelper.narrow(objectNameService);
-			NameComponent[] path = {new NameComponent("Server", "PhoneBook")};
-			
-			Object objectInterface = naming.resolve(path);
-			System.out.println("Obtido a interface");
-			this.server = PhoneBookServerInterfaceHelper.narrow(objectInterface);
-			
-		} catch (InvalidName e) {
-			e.printStackTrace();
-		} catch (NotFound e) {
-			e.printStackTrace();
-		} catch (CannotProceed e) {
-			e.printStackTrace();
-		} catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
-			e.printStackTrace();
+
+		Object objectInterface = seekReference();
+
+		if (objectInterface == null) {
+			return false;
 		}
+
+		this.server = PhoneBookServerInterfaceHelper.narrow(objectInterface);
+
+		synchronizeList();
+
+		return true;
 	}
-	
+
+	private Object seekReference(){
+		Object objectInterface = null;
+		int index = 0;
+
+		do {
+			try {
+
+				Object objectNameService = orb.resolve_initial_references("NameService");
+
+				NamingContext naming = NamingContextHelper.narrow(objectNameService);
+				NameComponent[] path = {new NameComponent("Server", "PhoneBook-"+index)};
+
+				objectInterface = naming.resolve(path);
+
+			} catch (InvalidName e) {
+				index++;
+				e.printStackTrace();
+			} catch (NotFound e) {
+				index++;
+				e.printStackTrace();
+			} catch (CannotProceed e) {
+				index++;
+				e.printStackTrace();
+			} catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
+				index++;
+				e.printStackTrace();
+			}
+		} while ((objectInterface == null) && (index  < 3));
+
+		return objectInterface;
+	}
+
 	public void insertContact(String contactName, int contactNumber) {
 		this.getListContacts().put(contactName, contactNumber);
 		this.server.insertContact(contactName, contactNumber);
@@ -79,14 +108,101 @@ public class PhoneBookClient {
 	/**
 	 * @return the listContacts
 	 */
-	public HashMap<String, Integer> getListContacts() {
+	public TreeMap<String, Integer> getListContacts() {
 		return listContacts;
 	}
 
 	/**
 	 * @param listContacts the listContacts to set
 	 */
-	public void setListContacts(HashMap<String, Integer> listContacts) {
+	private void setListContacts(TreeMap<String, Integer> listContacts) {
 		this.listContacts = listContacts;
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			synchronizeList();
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private void synchronizeList() {
+		try {
+			ByteArrayInputStream byteArrayInput = new ByteArrayInputStream(Base64.decodeBase64(server.getListContact()));
+			TreeMap<String, Integer> list = convertToTreeMap(byteArrayInput);
+			if (!this.listContacts.equals(list)) {
+				setListContacts(list);
+				setChanged(true);
+			}
+		} catch (Exception e) {
+			init();
+			synchronizeList();
+			e.printStackTrace();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	private TreeMap<String, Integer> convertToTreeMap(ByteArrayInputStream byteArrayInput) {
+		TreeMap<String, Integer> list = null;
+		try {
+			list = (TreeMap<String, Integer>) new ObjectInputStream(byteArrayInput).readObject();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	/**
+	 * @return the isUpdated
+	 */
+	public boolean isChanged() {
+		return isChanged;
+	}
+
+	/**
+	 * @param isUpdated the isUpdated to set
+	 */
+	public void setChanged(boolean isChanged) {
+		this.isChanged = isChanged;
+	}
+
+	/**
+	 * @param contact
+	 */
+	public void deleteContact(String contact) {
+		this.server.deleteContact(contact);
+	}
+
+	/**
+	 * @param text
+	 * @param contactNumber
+	 * @param valueAt
+	 * @param valueAt2
+	 */
+	public void updateContact(String newName, int newNumber, String oldName, int oldNumber) {
+		try {
+			if (!newName.equals(oldName)) {
+				this.server.updateContactName(newName, oldName);
+			}else if(newNumber != oldNumber) {
+				this.server.updateContactNumber(oldName, newNumber);
+			}
+		} catch (Exception e) {
+			init();
+			updateContact(newName, newNumber, oldName, oldNumber);
+			e.printStackTrace();
+		}
+	}
+
 }
--- phonebook-corba-client/src/graphics/FrameClient.java
+++ phonebook-corba-client/src/graphics/FrameClient.java
@@ -3,24 +3,32 @@
  */
 package graphics;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.table.DefaultTableModel;
 
 import controller.PhoneBookClient;
 
 import javax.swing.border.TitledBorder;
 import javax.swing.UIManager;
-
-import java.awt.Color;
-
 import javax.swing.JPanel;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.util.TreeMap;
 
 /**
  * @author Lucas Diego Reboucas Rocha
@@ -33,10 +41,21 @@ public class FrameClient {
 	private JTable table;
 	private PhoneBookClient client;
 	private DefaultTableModel model;
+	private JTextField textFieldNameContact;
+	private JTextField textFieldNumberContact;
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (Exception e) {
+		}
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -54,24 +73,13 @@ public class FrameClient {
 	 */
 	public FrameClient() {
 		initialize();
-		this.client = new PhoneBookClient("127.0.0.1", "900");
-		client.init();
-		client.insertContact("Lucas", 88770835);
-		client.insertContact("Diego", 97492417);
-		client.insertContact("Rocha", 32624153);
-		client.insertContact("Jamilly", 88770835);
-		client.insertContact("Bill", 97492417);
-		client.insertContact("Valdeco", 32624153);
-		client.insertContact("Nelda", 88770835);
-		client.insertContact("Nilda", 97492417);
-		client.insertContact("Regina", 32624153);
-		client.insertContact("Higor", 88770835);
-		client.insertContact("Kelly", 97492417);
-		client.insertContact("Arthur", 32624153);
-		for (String key : client.getListContacts().keySet()) {
-			String[] values = {key, this.client.getListContacts().get(key).toString()};
-			model.addRow(values);
+		this.client = new PhoneBookClient(JOptionPane.showInputDialog("Intruduza o endereço no Servidor:"));
+		if (!client.init()) {
+			JOptionPane.showMessageDialog(null, "Não foi possível contactar o servidor.\nPor favor verifique sua conexão e o endereço fornecido.");
 		}
+		new Thread(client).start();
+		refreshTable();
+		new Thread(new ScheduledTask()).start();
 	}
 
 	/**
@@ -79,43 +87,178 @@ public class FrameClient {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 450);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 450, 480);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		model = new DefaultTableModel(new Object[][] {},	new String[] {"Nome", "N\u00FAmero"}) {
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+			@SuppressWarnings("rawtypes")
 			Class[] columnTypes = new Class[] {
-					String.class, Integer.class
-				};
-				public Class getColumnClass(int columnIndex) {
-					return columnTypes[columnIndex];
-				}
-			}; 
+				String.class, Integer.class
+			};
+			@SuppressWarnings({ "rawtypes", "unchecked" })
+			public Class getColumnClass(int columnIndex) {
+				return columnTypes[columnIndex];
+			}
+			@Override  
+			public boolean isCellEditable(final int row, final int column) {  
+				return false;  
+			} 
+		}; 
 		frame.getContentPane().setLayout(null);
-		
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(null, "Agenda", TitledBorder.LEADING, TitledBorder.TOP, null, null));
-		panel.setBounds(214, 11, 210, 390);
-		panel.setLayout(null);
+
+		JPanel panelTable = new JPanel();
+		panelTable.setBorder(new TitledBorder(null, "Agenda", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelTable.setBounds(214, 25, 210, 390);
+		panelTable.setLayout(null);
 
 		table = new JTable();
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				int row = table.getSelectedRow();
+				textFieldNameContact.setText(model.getValueAt(row, 0).toString());
+				textFieldNumberContact.setText(model.getValueAt(row, 1).toString());
+			}
+		});
 		table.setModel(model);
-		table.setBounds(5, 20, 180, 330);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 22, 194, 360);
-		scrollPane.setLayout(null);
+		table.setBounds(10, 25, 175, 325);
+
+		JScrollPane scrollPane = new JScrollPane(table);
+		scrollPane.setBounds(10, 21, 194, 361);
 		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		
-		
-		JLabel lblNome = new JLabel("Nome");
-		lblNome.setBounds(10, 0, 46, 14);
-		scrollPane.add(lblNome);
-		
-		JLabel lblNmero = new JLabel("N\u00FAmero");
-		lblNmero.setBounds(106, 0, 46, 14);
-		scrollPane.add(lblNmero);
-		
-		scrollPane.add(table);
-		panel.add(scrollPane);
-		frame.getContentPane().add(panel);
+
+		panelTable.add(scrollPane);
+		frame.getContentPane().add(panelTable);
+
+		JPanel panelContact = new JPanel();
+		panelContact.setBorder(new TitledBorder(null, "Contato", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panelContact.setBounds(6, 25, 199, 236);
+		frame.getContentPane().add(panelContact);
+		panelContact.setLayout(null);
+
+		JLabel lblNameContact = new JLabel("Nome:");
+		lblNameContact.setBounds(6, 21, 55, 16);
+		panelContact.add(lblNameContact);
+
+		textFieldNameContact = new JTextField();
+		textFieldNameContact.setBounds(6, 49, 186, 28);
+		panelContact.add(textFieldNameContact);
+		textFieldNameContact.setColumns(10);
+
+		JLabel lblNumberContact = new JLabel("N\u00FAmero:");
+		lblNumberContact.setBounds(6, 89, 55, 16);
+		panelContact.add(lblNumberContact);
+
+		textFieldNumberContact = new JTextField();
+		textFieldNumberContact.setBounds(6, 110, 186, 31);
+		panelContact.add(textFieldNumberContact);
+		textFieldNumberContact.setColumns(10);
+
+		JButton btnAdicionar = new JButton("Adicionar");
+		btnAdicionar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if (!textFieldNameContact.getText().equals("") && !textFieldNameContact.getText().isEmpty()) {
+					String contactName = textFieldNameContact.getText();
+					int contactNumber = 0;
+					try {
+						contactNumber = Integer.parseInt(textFieldNumberContact.getText());
+					} catch (NumberFormatException e) {
+						e.printStackTrace();
+					}
+
+					if (verifyContact(contactName,contactNumber)) {
+						client.insertContact(contactName, contactNumber);
+						String[] values = { textFieldNameContact.getText(),
+								textFieldNumberContact.getText() };
+						model.addRow(values);
+						refreshTable();
+					}
+
+				}else {
+					JOptionPane.showMessageDialog(null, "Por favor digite um nome não nulo.");
+				}
+				textFieldNameContact.setText(null);
+				textFieldNumberContact.setText(null);
+			}
+
+			private boolean verifyContact(String contactName, int contactNumber) {
+				if (client.getListContacts().containsKey(contactName) || client.getListContacts().containsValue(contactNumber)) {
+					JOptionPane.showMessageDialog(null, "Desculpe mas esse nome já foi cadastrado.");
+					return false;
+				}else {
+					return true;
+				}
+			}
+		});
+		btnAdicionar.setBounds(6, 153, 90, 28);
+		panelContact.add(btnAdicionar);
+
+		JButton btnAtualizar = new JButton("Atualizar");
+		btnAtualizar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				int contactNumber = 0;
+				try {
+					contactNumber = Integer.parseInt(textFieldNumberContact.getText());
+				} catch (NumberFormatException e) {
+					e.printStackTrace();
+				}
+				client.updateContact(textFieldNameContact.getText(),contactNumber, model.getValueAt(table.getSelectedRow(), 0).toString(), Integer.parseInt(model.getValueAt(table.getSelectedRow(), 1).toString()));
+				textFieldNameContact.setText(null);
+				textFieldNumberContact.setText(null);
+			}
+		});
+		btnAtualizar.setBounds(102, 153, 90, 28);
+		panelContact.add(btnAtualizar);
+
+		JButton btnDeletar = new JButton("Deletar");
+		btnDeletar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				client.deleteContact(textFieldNameContact.getText());
+				model.removeRow(table.getSelectedRow());
+				textFieldNameContact.setText(null);
+				textFieldNumberContact.setText(null);
+			}
+		});
+		btnDeletar.setBounds(54, 189, 90, 28);
+		panelContact.add(btnDeletar);
+	}
+
+	/**
+	 * 
+	 */
+	private void refreshTable() {
+
+		TreeMap<String, Integer> list = client.getListContacts();
+		System.out.println("Tem " + model.getRowCount());
+		int count = model.getRowCount();
+		for (int i = count-1; i >= 0; i--) {
+			model.removeRow(i);
+		}
+		for (String key : list.keySet()) {
+			String [] values = {key, list.get(key).toString()};
+			model.addRow(values);
+		}
+
+	}
+
+	private class ScheduledTask implements Runnable {
+		public void run() {
+			while (true) {
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				if (client.isChanged()) {
+					client.setChanged(false);
+					refreshTable();
+				}
+			}
+		}
 	}
 }
